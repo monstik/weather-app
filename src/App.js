db27181382be54b9c13d23bd30b83d416a456081
monstrik <55989179+monstik@users.noreply.github.com>
2020-02-09 20:41:57 +0200

    сделал редизайн приложения =D

--- src/App.js
+++ src/App.js
@@ -1,99 +1,74 @@
-import React from 'react';
-import './App.css';
-import Form from "./components/Form";
-import WeatherNow from "./components/Weather";
-import WeatherDisplay from "./components/weather_display/WeatherDisplay";
-import WeatherBar from "./components/weather_bar/WeatherBar";
+import React, {useState} from 'react';
 
 
-const API_KEY = 'a29f5016da9a1b10d4c9971db54fd01e';       //апишка
+const api = {
+    key: "b71a3bf98a509019b0984b55ad712905",
+    base: "https://api.openweathermap.org/data/2.5/"
+};//апишка
 
-class App extends React.Component {
-    state = {
-        temp: undefined,
-        city: undefined,
-        country: undefined,
-        sunrise: undefined,
-        sunset: undefined,
-        isSuccess: undefined,
-        error: undefined
-    };
-    test = false;
-    getCity = async (e) => {   //метод для получения названия города введенного пользователем в форму
-        e.preventDefault();
-        let city = e.target.elements.city.value;  //получаем город который пользователь ввел в строку
+function App() {
 
-        await this.getWeatherNow(await this.getData(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`));
-        await this.getWeatherOnWeek(await this.getData(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=metric`));
+    const [query, setQuery] = useState('');
+    const [weather, setWeather] = useState({});
 
-    };
-
-
-    getWeatherNow = (data) => {  //получаем погоду сейчас
-        console.log(data);
-        let error = this.isError(data.cod); //проверяем data на наличиие кодов ошибки
-
-        if (!error) {
-            this.setState(
-                {
-                    temp: data.main.temp,
-                    city: data.name,
-                    country: data.sys.country,
-                    sunrise: data.sys.sunrise,
-                    sunset: data.sys.sunset,
-                    errorMessage: undefined,
-                    isSuccess: true
-
+    const search = evt => {
+        if (evt.key === "Enter") {
+            fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+                .then(res => res.json())
+                .then(result => {
+                    setWeather(result);
+                    setQuery('');
+                    console.log(result)
                 });
-
-        } else {
-            this.setState({
-                isSuccess: false,
-                error: error,
-            });
         }
-
-    };
-
-
-    isError = (code) => {   //сервер вернет 404 - значит город не найден, если 400, значит строка была пустая
-
-        if (code === '400') {      //если ответ был 400, записываем в состояние ошибку
-
-            return 'Ведите название города';
-
-        } else if (code === '404') {       //если ответ был 404, записываем в состояние ошибку
-
-            return 'Город не найден =(';
-
-        }
-        this.test = true;
-        return '';
-    };
-
-
-    getData = async (response) => {     //делаем асинхронный запрос погоды на сервер
-        let api_url = await fetch(response);        //получаем ответ от сервера
-        return await api_url.json();        //парсим ответ из json
-    };
-
-    getWeatherOnWeek = (data) => {  //получаем погоду на 5 дней вперед
-        console.log(data);
-
-    };
-
-
-    render() {
-        return (
-            <div className="App">
-                <h1>Погода</h1>
-                <Form weatherMethod={this.getCity}/>
-                {this.test && <WeatherBar/> }
-            </div>
-
-        );
     }
 
+
+    const dateBuilder = (d) => {
+        let months = ["January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"];
+        let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+        let day = days[d.getDay()];
+        let date = d.getDate();
+        let month = months[d.getMonth()];
+        let year = d.getFullYear();
+
+        return `${day} ${date} ${month} ${year}`;
+    };
+
+    return (
+        <div className={(typeof weather.main != "undefined") ? (weather.main.temp > 16 ? 'app warm' : 'app') : 'app'}>
+            <main>
+                <div className="search-box">
+                    <input
+                        type="text"
+                        className="search-bar"
+                        placeholder="Search..."
+                        onChange={e => setQuery(e.target.value)}
+                        value={query}
+                        onKeyPress={search}
+                    />
+                </div>
+                {typeof weather.main != "undefined" ? (
+                    <div>
+                        <div className="location-box">
+                            <div className="location">{weather.name}, {weather.sys.country}</div>
+                            <div className="date">{dateBuilder(new Date())}</div>
+                        </div>
+                        <div className="weather-box">
+                            <div className="temp">
+                                {Math.round(weather.main.temp)}°C
+                            </div>
+                            <div className="weather">{weather.weather[0].main}</div>
+                        </div>
+                    </div>
+                ) : ('')}
+
+
+            </main>
+        </div>
+    );
 
 }
 
